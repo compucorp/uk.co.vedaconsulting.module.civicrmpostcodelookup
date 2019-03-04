f6b670358110c61c85235e10f7c14516b2600f8c
Tunbola Ogunwande <tunbolawande@yahoo.com>
2019-03-04 15:33:46 +0100

    Add the PafPostcodeLookup entity to store PAF postcode lookup data.

---
+++ xml/schema/CRM/Civicrmpostcodelookup/PafPostcodeLookup.entityType.php
@@ -0,0 +1,11 @@
+<?php
+// This file declares a new entity type. For more details, see "hook_civicrm_entityTypes" at:
+// http://wiki.civicrm.org/confluence/display/CRMDOC/Hook+Reference
+return array (
+  0 => 
+  array (
+    'name' => 'PafPostcodeLookup',
+    'class' => 'CRM_Civicrmpostcodelookup_DAO_PafPostcodeLookup',
+    'table' => 'civicrm_paf_postcode_lookup',
+  ),
+);
